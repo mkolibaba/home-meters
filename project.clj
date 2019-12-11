7bd1dd9a26cc494d2337e6f4719c9ed543c4cab4
mkolibaba <avierdaner@gmail.com>
2019-12-11 23:52:13 +0300

    Fix tests

--- project.clj
+++ project.clj
@@ -20,4 +20,6 @@
   :target-path "target/%s"
   
   ; TODO remove uberjar creation if exists 
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             ; ring-mock is using for testing web applications
+             :dev {:dependencies [[ring/ring-mock "0.4.0"]]}})
--- test/home_meters/core_test.clj
+++ test/home_meters/core_test.clj
@@ -1,7 +1,14 @@
 (ns home-meters.core-test
   (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
             [home-meters.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest index-test
+  (testing "Index page"
+    (let [response (home-meters-app (mock/request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello"))))
+
+  (testing "Error page"
+    (let [response (home-meters-app (mock/request :get "/abc"))]
+      (is (= (:status response) 404)))))
