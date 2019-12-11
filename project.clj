787e85e8a098a8358bb6167a007696b1d794f286
mkolibaba <avierdaner@gmail.com>
2019-12-11 23:40:24 +0300

    Add dependencies and sample routes

--- project.clj
+++ project.clj
@@ -1,9 +1,23 @@
 (defproject home-meters "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Home Meters web application"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 ; compojure is a simple routing lib
+                 [compojure "1.6.1"]
+                 ; ring-defaults provides default settings for web application
+                 [ring/ring-defaults "0.3.2"]]
+
+  ; plugin for running 'ring' task
+  ; https://github.com/weavejester/lein-ring
+  :plugins [[lein-ring "0.12.5"]]
+
+  ; handler for 'ring' task
+  :ring {:handler home-meters.core/home-meters-app}
+
   :main ^:skip-aot home-meters.core
   :target-path "target/%s"
+  
+  ; TODO remove uberjar creation if exists 
   :profiles {:uberjar {:aot :all}})
--- src/home_meters/core.clj
+++ src/home_meters/core.clj
@@ -1,7 +1,20 @@
 (ns home-meters.core
-  (:gen-class))
+  (:require [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defroutes app-routes
+  (GET "/" [] "Hello")
+  (route/not-found "Error"))
+
+; declare app
+(def home-meters-app
+  ; refering to https://github.com/ring-clojure/ring-defaults/blob/master/src/ring/middleware/defaults.clj
+  ; wrap-defaults just inits some default settings for app-routes and
+  ; site-defaults provides this default settings 
+  (wrap-defaults app-routes site-defaults))
+
+; (defn -main
+;   "I don't do a whole lot ... yet."
+;   [& args]
+;   (println "Hello, World!"))
