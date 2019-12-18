619a895edd1876a4eed3214c031b54af62a3736a
mkolibaba <avierdaner@gmail.com>
2019-12-18 23:33:48 +0300

    Fix input in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import { terser } from 'rollup-plugin-terser';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/home_meters/ui/main.js',
+	input: 'src/ui/main.js',
 	output: {
 		sourcemap: true,
 		format: 'iife',
